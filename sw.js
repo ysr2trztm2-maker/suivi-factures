11da0e1016b72e92352b270f3c1e999d7bc63436
ysr2trztm2-maker <ysr2trztm2@privaterelay.appleid.com>
2026-03-13 13:00:22 +0100

    Ajout Service Worker

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'sf-2026-03-13-1259';
+const ASSETS = ['./', './index.html', './icon.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
+
+self.addEventListener('message', e => {
+  if (e.data === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
