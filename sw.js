2bc6de6882bc32b4d1504b56026393aaf52d0b21
ysr2trztm2-maker <ysr2trztm2@privaterelay.appleid.com>
2026-03-13 13:08:54 +0100

    SW skipWaiting immédiat + reload clients

--- sw.js
+++ sw.js
@@ -1,25 +1,26 @@
-const CACHE = 'sf-2026-03-13-1304';
+const CACHE = 'sf-2026-03-13-1308';
 const ASSETS = ['./', './index.html', './icon.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    ).then(() => {
+      self.clients.claim();
+      self.clients.matchAll({type:'window'}).then(clients => {
+        clients.forEach(c => c.postMessage('reload'));
+      });
+    })
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(cached => cached || fetch(e.request))
   );
-});
-
-self.addEventListener('message', e => {
-  if (e.data === 'skipWaiting') self.skipWaiting();
 });
